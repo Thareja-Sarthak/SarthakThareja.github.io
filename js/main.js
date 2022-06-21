34cf0f97c4e67ecd4b105fd24841b611c0a8f8e7
Thareja-Sarthak <thareja_sarthak@yahoo.com>
2022-06-21 21:39:08 +0530

    My Portfolio Website

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//Header Scroll
+let nav= document.querySelector(".navbar");
+window.onscroll=function(){
+    if(document.documentElement.scrollTop>20){
+        nav.classList.add("header-scrolled");
+    }
+    else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+//nav hide
+let navBar=document.querySelectorAll(".nav-link");
+let navCollapse=document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
+$(".progress-bar").animate({
+    width: "70%"
+}, 2500);
+
+//15:18
